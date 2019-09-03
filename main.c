92e0eb0151c2855fd9b642644be9d91d8de0b68e
toyakon <toyadayo@gmail.com>
2019-09-03 20:38:08 +0900

    parse create id

--- main.c
+++ main.c
@@ -4,22 +4,27 @@
 
 #include "base58.h"
 
+void create_id(int len, char* res)
+{
+    int i;
+    
+    srand(time(NULL));
 
+    for (i = 0; i < len; i++) {
+        res[i] = BASE58[rand() % sizeof(BASE58)];
+    }
+    res[i] = '\0';
+}
 int main(int argc, char** argv)
 {
-    int i, ind, max=8;
-    char id[58];
+    int max = 8;
+    char id[sizeof(BASE58)];
 
     if (argc > 1) {
         max = atoi(argv[1]);
     }
 
-    srand(time(NULL));
-    for (i = 0; i < max; i++) {
-        ind = rand() % 57;
-        id[i] = BASE58[ind];
-    }
-    id[i] = '\0';
+    create_id(max, id);
     printf("%s\n", id);
     return 0;
 }
